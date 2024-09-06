121e233e83c2a1ee45c22912b4efa16a73294bc3
João Vitor Marinho <jvmarinhohoracio@outlook.com>
2024-09-06 18:02:24 -0300

    Add files via upload

---
+++ atividaprojeto/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo:"Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira",
+        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que é braba ",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags:"judô judo judoca judoka"   
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta  brasileira que também é braba",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
